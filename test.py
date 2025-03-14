ec1e40d964954e6397d492c44dee9d8ba6733d48
leoliver <leosdoliver@gmail.com>
2025-03-13 21:32:07 -0300

    Adição do arquivo de teste

---
+++ test.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import unittest
+import requests
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_000_alunos_retorna_lista(self):
+        #pega a url /alunos, com o verbo get
+        r = requests.get('http://localhost:5000/alunos')
+
+        #o status code foi pagina nao encontrada?
+        if r.status_code == 404:
+            self.fail("voce nao definiu a pagina /alunos no seu server")
+
+        try:
+            obj_retornado = r.json()
+            #r.json() é o jeito da biblioteca requests
+            #de pegar o arquivo que veio e transformar
+            #em lista ou dicionario.
+            #Vou dar erro se isso nao for possivel
+        except:
+            self.fail("queria um json mas voce retornou outra coisa")
+
+        #no caso, tem que ser uma lista
+        self.assertEqual(type(obj_retornado),type([]))
+
+def runTests():
+        suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestStringMethods)
+        unittest.TextTestRunner(verbosity=2,failfast=True).run(suite)
+
+
+if __name__ == '__main__':
+    runTests()
