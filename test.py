5911324ab098605e2855a65176e482c07e67deee
RenoirBagi <renoirinvest@gmail.com>
2025-03-17 20:06:28 -0300

    Retirar comentários em test_000_alunos_retorna_lista

--- test.py
+++ test.py
@@ -5,24 +5,17 @@ import requests
 class TestStringMethods(unittest.TestCase):
 
     def test_000_alunos_retorna_lista(self):
-        #pega a url /alunos, com o verbo get
         r = requests.get('http://localhost:5000/alunos')
 
-        #o status code foi pagina nao encontrada?
         if r.status_code == 404:
             self.fail("voce nao definiu a pagina /alunos no seu server")
 
         try:
-            obj_retornado = r.json()
-            #r.json() é o jeito da biblioteca requests
-            #de pegar o arquivo que veio e transformar
-            #em lista ou dicionario.
-            #Vou dar erro se isso nao for possivel
+            retorno = r.json()
         except:
             self.fail("queria um json mas voce retornou outra coisa")
 
-        #no caso, tem que ser uma lista
-        self.assertEqual(type(obj_retornado),type([]))
+        self.assertEqual(type(retorno),type([]))
 
 def runTests():
         suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestStringMethods)
