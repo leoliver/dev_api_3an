30a7fb942f2875ac0863f3525a242cba43385caf
sdineijr <sdineijr0@gmail.com>
2025-04-08 22:38:40 -0300

    Desenvolvimento da biblioteca alunos

---
+++ alunos/alunos_model.py
@@ -0,0 +1,38 @@
+from auxiliar import *
+
+dici = {
+    "alunos":[
+        {"id": 1,
+         "nome": "Sid",
+         "idade": 28,
+         "data_nascimento": "ontem",
+         "nota_primeiro_semestre": 0,
+         "nota_segundo_semestre": 0,
+         "media_final": 0,
+         "turma_id": 1},
+        {"id": 2,
+         "nome": "Leonardo",
+         "idade": 23,
+         "data_nascimento": "anteontem",
+         "nota_primeiro_semestre": 0,
+         "nota_segundo_semestre": 0,
+         "media_final": 0,
+         "turma_id": 1},
+        {"id": 3,
+         "nome": "Joana",
+         "idade": 23,
+         "data_nascimento": "há um tempo",
+         "nota_primeiro_semestre": 0,
+         "nota_segundo_semestre": 0,
+         "media_final": 0,
+         "turma_id": 1},
+        {"id": 4,
+         "nome": "Renoir",
+         "idade": 19,
+         "data_nascimento": "Amanhã",
+         "nota_primeiro_semestre": 0,
+         "nota_segundo_semestre": 0,
+         "media_final": 0,
+         "turma_id": 1}
+        ],
+}
