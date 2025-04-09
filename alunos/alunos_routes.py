356451752d13170237186eafd1f7bca1f6d7a6da
sdineijr <sdineijr0@gmail.com>
2025-04-08 22:49:44 -0300

    Desenvolvimento da rota getAlunoById

--- alunos/alunos_routes.py
+++ alunos/alunos_routes.py
@@ -7,3 +7,11 @@ alunos_blueprint = Blueprint('alunos', __name__)
 def get_alunos():
     alunos = getAlunos()
     return jsonify(alunos)
+
+@alunos_blueprint.route('/alunos/<int:idAluno>', methods=['GET'])
+def getalunobyid(idAluno):
+    try:
+        aluno = getAlunoById(idAluno)
+        return jsonify(aluno)
+    except AlunosNaoEncontrados:
+        return jsonify({"erro": "Aluno nao encontrado"})
