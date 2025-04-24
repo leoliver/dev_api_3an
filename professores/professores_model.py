13a6c46d4ce5546900b8584cb9704039f75ce7f6
leoliver <leosdoliver@gmail.com>
2025-04-23 21:45:33 -0300

    fix/Correção de testes e model de Professor

--- professores/professores_model.py
+++ professores/professores_model.py
@@ -7,12 +7,14 @@ class Professor(db.Model):
     nome = db.Column(db.String(100))
     idade = db.Column(db.Integer)
     data_nascimento = db.Column(db.Date)
+    disciplina = db.Column(db.String(100))
     salario = db.Column(db.Float)
 
-    def __init__(self, nome, data_nascimento, salario):
+    def __init__(self, nome, data_nascimento, disciplina, salario):
         self.nome = nome
         self.data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y").date()
         self.idade = self.calcular_idade(self.data_nascimento)
+        self.disciplina = disciplina
         self.salario = salario
     
     def calcular_idade(self, nascimento):
@@ -24,6 +26,7 @@ class Professor(db.Model):
          "nome": self.nome,
          "idade": self.idade,
          "data_nascimento": self.data_nascimento.strftime("%d-%m-%Y"),
+         "disciplina": self.disciplina,
          "salario": self.salario}
 
 class ProfessoresNaoEncontrados(Exception):
@@ -43,7 +46,7 @@ def getProfessorById(idProfessor):
     return professor.to_dict()
 
 def createProfessor(dados):
-    novoProfessor = Professor(nome=dados['nome'], data_nascimento=dados['data_nascimento'],salario=dados['salario'])
+    novoProfessor = Professor(nome=dados['nome'], data_nascimento=dados['data_nascimento'], disciplina=dados["disciplina"], salario=dados['salario'])
     db.session.add(novoProfessor)
     db.session.commit()
     return novoProfessor.to_dict()
