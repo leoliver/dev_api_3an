5174bd3b4826090f35a9997e298cb2bb05e8ec95
Joana Rosa <jjnn.rosa19@gmail.com>
2025-04-23 22:17:38 -0300

    fix/Correção teste da entidade turma

--- test.py
+++ test.py
@@ -83,158 +83,164 @@ class TestStringMethods(unittest.TestCase):
      #      if not deletado:
      #           self.fail("O aluno não foi deletado corretamente.")
 
-     def test_100_professores_retorna_lista(self):
-          r = requests.get('http://localhost:5000/professores')
+     # def test_100_professores_retorna_lista(self):
+     #      r = requests.get('http://localhost:5000/professores')
 
-          if r.status_code == 404:
-               self.fail("Página /professores não definida no servidor para o método GET.")
+     #      if r.status_code == 404:
+     #           self.fail("Página /professores não definida no servidor para o método GET.")
 
-          try:
-               professores = r.json()
-          except:
-               self.fail("Formato deveria ser JSON")
-
-          self.assertEqual(type(professores),type([])) 
-
-     def test_101_professores_procura_id(self):
-          r = requests.get('http://localhost:5000/professores/1')
-          
-          if r.status_code == 404:
-               self.fail("Página /professores/id não definida no servidor para o método GET por Id.")
-
-          try:
-               professor = r.json()
-          except:
-               self.fail("Formato deveria ser JSON.")
-
-          self.assertEqual(type(professor),type({}))
-
-     def test_102_professores_criacao(self):
-          r = requests.post('http://localhost:5000/professores', json={'nome': 'Sidinei', 'data_nascimento': '07-08-1997', 'disciplina':'Matematica', 'salario': 3000})
-
-          if r.status_code == 404:
-               self.fail("Página /professores não definida no servidor para o método POST.")
-
-          resposta = requests.get('http://localhost:5000/professores')
-          achei = False
-          lista_professores = resposta.json()
-
-          for professor in lista_professores:
-               if professor['nome'] == 'Sidinei':
-                    achei = True
-          
-          if not achei:
-               self.fail("O professor não foi encontrado")
-
-     def test_103_professores_atualiza_dados_id(self):
-          get = requests.get('http://localhost:5000/professores')
-          lista_professores = get.json()
-          lastIndex = lista_professores[-1]["id"]
-
-          r = requests.put(f'http://localhost:5000/professores/{lastIndex}', json={'disciplina':'Desenvolvimento Mobile'})
-
-          if r.status_code == 404:
-               self.fail("Página /professores/id não definida no servidor para o método PUT.")
-
-          resposta = requests.get(f'http://localhost:5000/professores/{lastIndex}')
-          professor = resposta.json()
-          if not professor['disciplina'] == 'Desenvolvimento Mobile':
-               self.fail("Dados não foram atualizados")
-
-     def test_104_professores_delete_id(self):
-          get = requests.get('http://localhost:5000/professores')
-          lista_professores = get.json()
-          lastIndex = lista_professores[-1]["id"]
-
-          r = requests.delete(f'http://localhost:5000/professores/{lastIndex}')
-          
-          if r.status_code == 404:
-               self.fail("Página /professores/id não definida no servidor para o método DELETE.")
-     
-          resposta = requests.get('http://localhost:5000/professores')
-          lista_professores = resposta.json()
-          deletado = True
-
-          for professor in lista_professores:
-               if professor['id'] == lastIndex:
-                    deletado = False
-
-          if not deletado:
-               self.fail("O professor não foi deletado corretamente.")
-
-     #Testes Turmas     
-     # def test_200_busca_as_turmas(self):
-
-     #      req_turmas = requests.get('http://127.0.0.1:5000/turmas')
-     #      if req_turmas.status_code == 404:
-     #                self.fail("Página /turmas não definida no servidor para o método GET.")
-               
      #      try:
-     #           retorno = req_turmas.json()
-               
+     #           professores = r.json()
      #      except:
-     #           self.fail("Seu objeto não foi convertido para Json.")
+     #           self.fail("Formato deveria ser JSON")
+
+     #      self.assertEqual(type(professores),type([])) 
+
+     # def test_101_professores_procura_id(self):
+     #      r = requests.get('http://localhost:5000/professores/1')
           
-     #      self.assertEqual(type(retorno), type([]))
-     
-     # def test_201_busca_turma_por_ID(self):
-     #      req_turmas = requests.get('http://127.0.0.1:5000/turmas/1')
-     #      retorno = req_turmas.json()
-     #      if req_turmas.status_code == 404:
-     #                self.fail("Página /turmas/id não definida no servidor para o método GET.")
+     #      if r.status_code == 404:
+     #           self.fail("Página /professores/id não definida no servidor para o método GET por Id.")
 
-     #      self.assertEqual(retorno['id'],1)
-     
-     # def test_202_adiciona_turma(self):
-     #      req_turmas = requests.post('http://127.0.0.1:5000/turmas',json={"nome": "Engenharia de Requisitos", "turno": "Manhã", "id_professor": 3})
-     #      req_turmas2 = requests.get('http://127.0.0.1:5000/turmas')
-     #      if req_turmas.status_code == 404:
-     #                self.fail("Página /turmas não definida no servidor para o método POST.")
+     #      try:
+     #           professor = r.json()
+     #      except:
+     #           self.fail("Formato deveria ser JSON.")
 
-     #      lista_turmas = req_turmas2.json()
+     #      self.assertEqual(type(professor),type({}))
+
+     # def test_102_professores_criacao(self):
+     #      r = requests.post('http://localhost:5000/professores', json={'nome': 'Sidinei', 'data_nascimento': '07-08-1997', 'disciplina':'Matematica', 'salario': 3000})
+
+     #      if r.status_code == 404:
+     #           self.fail("Página /professores não definida no servidor para o método POST.")
+
+     #      resposta = requests.get('http://localhost:5000/professores')
      #      achei = False
+     #      lista_professores = resposta.json()
 
-     #      for turma in lista_turmas:
-     #           if turma["nome"] == "Engenharia de Requisitos":
+     #      for professor in lista_professores:
+     #           if professor['nome'] == 'Sidinei':
      #                achei = True
-
+          
      #      if not achei:
-     #           self.fail("Não achei o objeto adicionado.")        
+     #           self.fail("O professor não foi encontrado")
 
-     # def test_203_atualiza_turma(self):
-     #      req_turmas = requests.put('http://127.0.0.1:5000/turmas/1', json = {"nome": "Lógica de Programação"})
-     #      req_turmas_att = requests.get('http://127.0.0.1:5000/turmas')
-     #      if req_turmas.status_code == 404:
-     #                self.fail("Página /turmas não definida no servidor para o método PUT.")
+     # def test_103_professores_atualiza_dados_id(self):
+     #      get = requests.get('http://localhost:5000/professores')
+     #      lista_professores = get.json()
+     #      lastIndex = lista_professores[-1]["id"]
 
-     #      retorno = req_turmas_att.json()
-     #      atualizado = False
+     #      r = requests.put(f'http://localhost:5000/professores/{lastIndex}', json={'disciplina':'Desenvolvimento Mobile'})
 
-     #      for turma in retorno:
-     #           if turma["nome"] == "Lógica de Programação":
-     #                atualizado = True
-               
-     #      if not atualizado:
-     #           self.fail("Disciplina não atualizada.")
+     #      if r.status_code == 404:
+     #           self.fail("Página /professores/id não definida no servidor para o método PUT.")
 
-     #      retorno = req_turmas_att.json()
+     #      resposta = requests.get(f'http://localhost:5000/professores/{lastIndex}')
+     #      professor = resposta.json()
+     #      if not professor['disciplina'] == 'Desenvolvimento Mobile':
+     #           self.fail("Dados não foram atualizados")
 
-     # def test_204_deleta_turma(self):
-     #      req_turmas = requests.post('http://127.0.0.1:5000/turmas', json={"nome": "Engenharia de Requisitos", "turno": "Manhã", "id_professor":2})
-     #      req_turmas3 = requests.delete('http://127.0.0.1:5000/turmas/4')
-     #      req_turmas2 = requests.get('http://127.0.0.1:5000/turmas')
-     #      if req_turmas.status_code == 404:
-     #                self.fail("Página /turmas não definida no servidor para o método DELETE.")
+     # def test_104_professores_delete_id(self):
+     #      get = requests.get('http://localhost:5000/professores')
+     #      lista_professores = get.json()
+     #      lastIndex = lista_professores[-1]["id"]
 
-     #      lista_turmas = req_turmas2.json()
+     #      r = requests.delete(f'http://localhost:5000/professores/{lastIndex}')
+          
+     #      if r.status_code == 404:
+     #           self.fail("Página /professores/id não definida no servidor para o método DELETE.")
+     
+     #      resposta = requests.get('http://localhost:5000/professores')
+     #      lista_professores = resposta.json()
      #      deletado = True
 
-     #      for turma in lista_turmas:
-     #           if turma["id"] == 4:
+     #      for professor in lista_professores:
+     #           if professor['id'] == lastIndex:
      #                deletado = False
-          
+
      #      if not deletado:
-     #           self.fail("A turma não foi deletada.")     
+     #           self.fail("O professor não foi deletado corretamente.")
+
+     #Testes Turmas     
+     def test_200_busca_as_turmas(self):
+
+          req_turmas = requests.get('http://127.0.0.1:5000/turmas')
+          if req_turmas.status_code == 404:
+                    self.fail("Página /turmas não definida no servidor para o método GET.")
+               
+          try:
+               retorno = req_turmas.json()
+               
+          except:
+               self.fail("Seu objeto não foi convertido para Json.")
+          
+          self.assertEqual(type(retorno), type([]))
+     
+     def test_201_busca_turma_por_ID(self):
+          req_turmas = requests.get('http://127.0.0.1:5000/turmas/1')
+          retorno = req_turmas.json()
+          if req_turmas.status_code == 404:
+                    self.fail("Página /turmas/id não definida no servidor para o método GET.")
+
+          self.assertEqual(retorno['id'],1)
+     
+     def test_202_adiciona_turma(self):
+          req_turmas = requests.post('http://127.0.0.1:5000/turmas',json={"nome": "Engenharia de Requisitos", "turno": "Manhã", "id_professor": 3})
+          req_turmas2 = requests.get('http://127.0.0.1:5000/turmas')
+          if req_turmas.status_code == 404:
+                    self.fail("Página /turmas não definida no servidor para o método POST.")
+
+          lista_turmas = req_turmas2.json()
+          achei = False
+
+          for turma in lista_turmas:
+               if turma["nome"] == "Engenharia de Requisitos":
+                    achei = True
+
+          if not achei:
+               self.fail("Não achei o objeto adicionado.")        
+
+     def test_203_atualiza_turma(self):
+          get = requests.get('http://127.0.0.1:5000/turmas')
+          lista_turmas = get.json()
+          last_index = lista_turmas[-1]['id']
+          req_turmas = requests.put(f'http://127.0.0.1:5000/turmas/{last_index}', json = {"nome": "Lógica de Programação"})
+          req_turmas_att = requests.get('http://127.0.0.1:5000/turmas')
+          if req_turmas.status_code == 404:
+                    self.fail("Página /turmas não definida no servidor para o método PUT.")
+
+          retorno = req_turmas_att.json()
+          atualizado = False
+
+          for turma in retorno:
+               if turma["nome"] == "Lógica de Programação":
+                    atualizado = True
+               
+          if not atualizado:
+               self.fail("Disciplina não atualizada.")
+
+          retorno = req_turmas_att.json()
+
+     def test_204_deleta_turma(self):
+          get = requests.get('http://127.0.0.1:5000/turmas')
+          lista_turmas = get.json()
+          last_index = lista_turmas[-1]['id']
+          req_turmas = requests.post('http://127.0.0.1:5000/turmas', json={"nome": "Engenharia de Requisitos", "turno": "Manhã", "id_professor":2})
+          req_turmas3 = requests.delete(f'http://127.0.0.1:5000/turmas/{last_index}')
+          req_turmas2 = requests.get('http://127.0.0.1:5000/turmas')
+          if req_turmas.status_code == 404:
+                    self.fail("Página /turmas não definida no servidor para o método DELETE.")
+
+          lista_turmas = req_turmas2.json()
+          deletado = True
+
+          for turma in lista_turmas:
+               if turma["id"] == last_index:
+                    deletado = False
+          
+          if not deletado:
+               self.fail("A turma não foi deletada.")     
                         
 
 def runTests():
