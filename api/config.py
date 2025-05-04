a957841a745ea11c2a3469a82d2d12c7013b2973
leoliver <leosdoliver@gmail.com>
2025-05-04 18:10:37 -0300

    fix/Correção portas render

--- api/config.py
+++ api/config.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['HOST'] = '127.0.0.1'
-app.config['PORT'] = 5000
+app.config['HOST'] = '0.0.0.0'
+app.config['PORT'] = int(os.environ.get("PORT", 5000))
 app.config['DEBUG'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
